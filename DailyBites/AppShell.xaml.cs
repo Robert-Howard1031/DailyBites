0e6b706342b57a29ab90f97c4740b0d5f0279b8f
Robert Howard <robertleehoward1031@gmail.com>
2025-09-20 14:03:06 -0500

    Add project files.

---
+++ DailyBites/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyBites
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DailyBites/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DailyBites
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
